537c7e3f60decb28d178907b19b8c3a6889f20d7
bulan <bulan.athaillah@gmail.com>
2023-03-13 14:45:06 +0700

    fix: added attributes, constructor, and methods

--- assignment2/src/main/java/assignments/assignment2/Member.java
+++ assignment2/src/main/java/assignments/assignment2/Member.java
@@ -4,9 +4,28 @@ import assignments.assignment1.NotaGenerator;
 
 public class Member {
     // TODO: tambahkan attributes yang diperlukan untuk class ini
-    public Member(String nama, String noHp) {
+    private String nama;
+    private String noHp;
+    private String id;
+    private int bonusCounter;
+
+    public Member(String nama, String noHp, String id) {
         // TODO: buat constructor untuk class ini
+        this.nama = nama;
+        this.noHp = noHp;
+        this.id = id;
     }
 
     // TODO: tambahkan methods yang diperlukan untuk class ini
+    public String getNama() {
+        return this.nama;
+    }
+
+    public String getNoHP() {
+        return this.noHp;
+    }
+
+    public String getId() {
+        return this.id;
+    }
 }
