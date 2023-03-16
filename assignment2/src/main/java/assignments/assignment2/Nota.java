b03d1e8f749784db4b269815eb335e64c0349474
bulan <bulan.athaillah@gmail.com>
2023-03-16 09:40:58 +0700

    fix: added methods and string output

--- assignment2/src/main/java/assignments/assignment2/Nota.java
+++ assignment2/src/main/java/assignments/assignment2/Nota.java
@@ -1,9 +1,10 @@
 package assignments.assignment2;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import assignments.assignment1.NotaGenerator;
 
 public class Nota {
-    // TODO: tambahkan attributes yang diperlukan untuk class ini
     private int idNota;
     private String paket;
     private Member member;
@@ -12,13 +13,56 @@ public class Nota {
     private int sisaHariPengerjaan;
     private boolean isReady;
 
-    public Nota(Member member, String paket, int berat, String tanggalMasuk) {
-        // TODO: buat constructor untuk class ini
+    public Nota(Member member, String paket, int berat, String tanggalMasuk, int idNota, int sisaHariPengerjaan) {
         this.member = member;
         this.paket = paket;
         this.berat = berat;
         this.tanggalMasuk = tanggalMasuk;
+        this.idNota = idNota;
+        this.sisaHariPengerjaan = sisaHariPengerjaan;
     }
 
-    // TODO: tambahkan methods yang diperlukan untuk class ini
+    public int getIdNota() {
+        return this.idNota;
+    }
+
+    public int getSisaHariPengerjaan() {
+        return this.sisaHariPengerjaan;
+    }
+
+    public void setSisaHariPengerjaan() {
+        this.sisaHariPengerjaan--;
+    };
+
+    public boolean getIsReady() {
+        return this.isReady;
+    }
+
+    public void setIsReady() {
+        this.isReady = true;
+    }
+
+    public String toString() {
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar cal = Calendar.getInstance();
+        int year = Integer.parseInt(tanggalMasuk.substring(6));
+        int month = Integer.parseInt(tanggalMasuk.substring(3, 5)) - 1;
+        int date = Integer.parseInt(tanggalMasuk.substring(0, 2));
+        cal.set(year, month, date);
+        cal.add(Calendar.DATE, sisaHariPengerjaan);
+
+        long harga = NotaGenerator.getHargaPaket(paket);
+        String nota = "";
+
+        nota += String.format("[ID Nota = %d]\n", idNota);
+        nota += String.format("ID	: %s\n", this.member.getId());
+        nota += String.format("Paket : %s\n", paket);
+        nota += "Harga :\n";
+        nota += String.format("%d kg x %d = %d\n", berat, harga, berat*harga);
+        nota += String.format("Tanggal Terima  : %s\n", tanggalMasuk);
+        nota += String.format("Tanggal Selesai : %s\n", fmt.format(cal.getTime()));
+        nota += "Status      	: Belum bisa diambil :(\n";
+
+        return nota;
+    }
 }
