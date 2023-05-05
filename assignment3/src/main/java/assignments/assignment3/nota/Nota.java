6809cabb920bb111fb7c5475b5992127187bb3e6
bulan <bulan.athaillah@gmail.com>
2023-05-05 22:19:06 +0700

    fix: added methods

--- assignment3/src/main/java/assignments/assignment3/nota/Nota.java
+++ assignment3/src/main/java/assignments/assignment3/nota/Nota.java
@@ -1,52 +1,138 @@
 package assignments.assignment3.nota;
+import assignments.assignment1.NotaGenerator;
+import assignments.assignment3.nota.service.AntarService;
+import assignments.assignment3.nota.service.CuciService;
 import assignments.assignment3.nota.service.LaundryService;
+import assignments.assignment3.nota.service.SetrikaService;
 import assignments.assignment3.user.Member;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+
 public class Nota {
     private Member member;
     private String paket;
-    private LaundryService[] services;
+    private LaundryService[] services = new LaundryService[0];
     private long baseHarga;
+    private long harga;
     private int sisaHariPengerjaan;
-    private  int berat;
+    private int berat;
     private int id;
     private String tanggalMasuk;
     private boolean isDone;
+    private boolean terlambat;
     static public int totalNota;
 
-    public Nota(Member member, int berat, String paket, String tanggal) {
+    public Nota(Member member, int berat, String paket, String tanggalMasuk) {
         //TODO
+        this.member = member;
+        this.berat = berat;
+        this.paket = paket;
+        this.tanggalMasuk = tanggalMasuk;
+
+        sisaHariPengerjaan = NotaGenerator.getHariPaket(paket);
+        harga = NotaGenerator.getHargaPaket(paket);
     }
 
-    public void addService(LaundryService service){
+    public void addService(LaundryService service) {
         //TODO
+        services = Arrays.copyOf(services, services.length + 1);
+        services[services.length - 1] = service;
     }
 
-    public String kerjakan(){
+    public String kerjakan() {
         // TODO
-        return "";
+        String mengerjakan = "";
+        for (LaundryService service: services) {
+            if (service instanceof AntarService) {
+                LaundryService antar = new AntarService();
+                mengerjakan = antar.doWork();
+            } else if (service instanceof CuciService) {
+                LaundryService cuci = new CuciService();
+                mengerjakan = cuci.doWork();
+            } else {
+                LaundryService setrika = new SetrikaService();
+                mengerjakan = setrika.doWork();
+            }
+        }
+        return mengerjakan;
     }
+
     public void toNextDay() {
         // TODO
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 1);
     }
 
-    public long calculateHarga(){
+    public long calculateHarga() {
         // TODO
-        return -1;
+        baseHarga += getHarga() * getBerat();
+
+        for (LaundryService service: services) {
+            baseHarga += service.getHarga(getBerat());
+        }
+
+        if (!isDone) {
+            if (sisaHariPengerjaan < 0) {
+                baseHarga -= (Math.abs(getSisaHariPengerjaan()) * 2000);
+                terlambat = true;
+            }
+        }
+    
+        return baseHarga;
     }
 
-    public String getNotaStatus(){
-        // TODO
-        return "";
+    public String getNotaStatus() {
+        if (!isDone()) {
+            return "Belum selesai.";
+        }
+        return "Sudah selesai.";
     }
 
+    public void setIdNota(int id) {
+        this.id = id;
+    }
+
+    public void setSisaHariPengerjaan() {
+        this.sisaHariPengerjaan--;
+    }
+    
     @Override
-    public String toString(){
-        // TODO
-        return "";
+    public String toString() {
+        //TODO: masih salah ngitung tanggal
+        //Menginisiasi class SimpleDateFormat dan Calendar untuk menghitung tanggalSelesai
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar cal = Calendar.getInstance();
+        
+        //Menyesuaikan tanggalMasuk ke format Calendar
+        int year = Integer.parseInt(tanggalMasuk.substring(6));
+        int month = Integer.parseInt(tanggalMasuk.substring(3, 5)) - 1;
+        int date = Integer.parseInt(tanggalMasuk.substring(0, 2));
+        cal.set(year, month, date);
+        
+        //Menambah hari sesuai dengan paket laundry
+        cal.add(Calendar.DATE, sisaHariPengerjaan);
+        
+        String nota = "";
+        nota += String.format("[ID Nota = %d]\n", getIdNota());
+        nota += String.format("ID   : %s\n", this.member.getId());
+        nota += String.format("Paket : %s\n", getPaket());
+        nota += "Harga :\n";
+        nota += String.format("%d kg x %d = %d\n", getBerat(), getHarga(), getBerat() * getHarga());
+        nota += String.format("Tanggal Terima  : %s\n", getTanggal());
+        nota += String.format("Tanggal Selesai : %s\n", fmt.format(cal.getTime()));
+        nota += String.format("--- SERVICE LIST ---\n");
+        for (LaundryService service: services) {
+            nota += String.format("- %s @ Rp.%d\n", service.getServiceName(), service.getHarga(getBerat()));
+        }
+        nota += String.format("Harga Akhir: %d", calculateHarga());
+        if (terlambat) {
+            nota += String.format("Ada kompensasi keterlambatan %d * 2000 hari\n", Math.abs(sisaHariPengerjaan));
+        }
+        return nota;
     }
-
+    
     // Dibawah ini adalah getter
-
     public String getPaket() {
         return paket;
     }
@@ -59,14 +145,23 @@ public class Nota {
         return tanggalMasuk;
     }
 
-    public int getSisaHariPengerjaan(){
+    public int getSisaHariPengerjaan() {
         return sisaHariPengerjaan;
     }
+    
     public boolean isDone() {
         return isDone;
     }
 
-    public LaundryService[] getServices(){
+    public LaundryService[] getServices() {
         return services;
+    }
+
+    public long getHarga() {
+        return harga;
+    }
+
+    public int getIdNota() {
+        return id;
     }
 }
